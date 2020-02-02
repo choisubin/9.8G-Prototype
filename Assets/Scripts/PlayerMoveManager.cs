e5b7a1097195f303fd6ca32678a8122613d92adb
DDingSu <pmss626@naver.com>
2020-02-02 18:35:24 +0900

    move player

---
+++ Assets/Scripts/PlayerMoveManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveManager : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 1.0f;
+
+    private Vector3 movement = Vector3.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement.Set(moveHorizontal, 0, moveVertical);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        //rb.MovePosition(transform.position + movement);
+
+        transform.Translate(movement);
+    }
+}
